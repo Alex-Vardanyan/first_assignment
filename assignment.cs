ae6710993898b066b8fdcefabb3143d532d6bd30
Alex-Vardanyan <88556305+Alex-Vardanyan@users.noreply.github.com>
2021-08-07 10:44:54 +0400

    Add files via upload

---
+++ assignment.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace FirstAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Main function!");
+            Console.ReadLine();
+        }
+
+        static void SomeFunc(string[] args)
+        {
+            Console.WriteLine("Hello World from some other function too");
+            Console.ReadLine();
+        }
+    }
+}
